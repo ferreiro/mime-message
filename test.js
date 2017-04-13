39edc6da1cceabf0e7646d93e415d8357ef762b6
Jorge Ferreiro <jorge@ferreiro.me>
2017-04-13 05:18:51 +0200

    Added support for reply-to, CC, BCC and ToMany

--- test.js
+++ test.js
@@ -11,6 +11,11 @@ const message = mimeMessage.createMimeMessage({
     name: 'Jorge Ferreiro',
     email: 'jorge@ferreiro.me'
   },
+  toMany: 'Susan <susan@example.com>, Paul <paul@example.com>',
+  replyTo: {
+    name: 'Test',
+    email: '<test@test.com>'
+  },
   date: 'Date: Fri, 21 Nov 1997 09:55:06 -0600',
   subject: 'Hello World!',
   message: '<h1>Hello from the other side!</h1><p>This is pretty awesome!</p>'
